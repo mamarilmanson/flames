879954512f5f0097033f2f11d30170c051a76003
Manson Mamaril <manson_jabba@yahoo.com>
2015-06-27 15:13:40 +0800

    Original files from 2011...

---
+++ about.php
@@ -0,0 +1,27 @@
+<table id="table" border="0">
+<tr>
+		<td style="height:200">
+	<span style="font-family:arial; font-size: 20px; color: black">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>F.L.A.M.E.S.</b> is a favorite past time in the Philippines which means Friends, Lovers, Anger, Married, Engaged and Sweethearts.
+It starts with writing two names in a piece of paper, your name and the name of the one whom you admire - may be a crush - then the rule is to count all the similar letters within your name, then get the total, 
+after that count from 1 until you reach the total number of similar letters where <b>F</b> is 1, <b>L</b> is 2 and so on. <br/><br/>View this example: </span>
+		</td>
+	</tr>
+	<tr>
+		<td style="height:250">
+		<div align="center">
+		<table width="400" height="100" border="0">
+		<tr>
+			<td height="40"><div align="center" id="result">Br<span class="let">a</span>d p<span class="let">i</span>tt</div></td>
+		</tr>
+		<tr>
+			<td height="40"><div align="center" id="result"><span class="let">A</span>ngel<span class="let">i</span>n<span class="let">a</span> Jol<span class="let">i</span>e</div></td>
+		</tr>
+		<tr>
+			<td height="30"><div align="center"><span style="font-family: old english text mt; font-size: 20px; color: black"></span></div></td>
+		</tr>
+		</table>
+		</div>
+	</td>
+</tr>
+</table>
---
+++ disclaimer.php
@@ -0,0 +1,37 @@
+<table id="table" border="0">
+<tr>
+	<td height="height: 20px">
+		<div align="center"><span style="font-size: 20px; font-family: old english text mt; color: black">Disclaimer:</span></div>
+	</td>
+</tr>
+<tr>
+	<td style="height: 30px">
+	<div align="center"><span class="txts">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This website does <b>NOT</b> save any entered name, although, it saves the result but only appear
+ as digits from 0 - 5 as illustrated below: </span></div>
+	</td>
+</tr>
+<tr>
+	<td style="height: 300px">
+	<div align="center">
+	<table width="220" id="dis" border="0">
+		<tr><td width="100">Friends</td><td>...................</td><td width="20">0</td></tr>
+		<tr><td width="100">Lovers</td><td>...................</td><td width="20">1</td></tr>
+		<tr><td width="100">Anger</td><td>...................</td><td width="20">2</td></tr>
+		<tr><td width="100">Married</td><td>...................</td><td width="20">3</td></tr>
+		<tr><td width="100">Engaged</td><td>...................</td><td width="20">4</td></tr>
+		<tr><td width="100">Sweethearts</td><td>...................</td><td width="20">5</td></tr>
+	</table>
+	</div>
+	</td>
+</tr>
+<tr>
+	<td style="height: 100px">
+	<div align="center">
+		<span class="txts">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;So what is stored inside count.txt is only a straight stream of numbers, e.g:<br/>
+					<br/><b>count.txt</b> >> 0120340123412301 ...
+		</span>
+	</div>
+	</td>
+</tr>
+</table>
