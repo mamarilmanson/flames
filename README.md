cecdf4198ea4ed7be3270f7856960760737328d6
Manson Mamaril <manson_jabba@yahoo.com>
2015-06-27 17:30:26 +0800

    Another update...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # F.L.A.M.E.S.
 Reviving a small test project from 2011...
+
+http://www.flames.totalh.net/
--- disclaimer.php
+++ disclaimer.php
@@ -1,19 +1,6 @@
-<table id="table" border="0">
-<tr>
-	<td height="height: 20px">
-		<div align="center"><span style="font-size: 20px; font-family: old english text mt; color: black">Disclaimer:</span></div>
-	</td>
-</tr>
-<tr>
-	<td style="height: 30px">
-	<div align="center"><span class="txts">
-		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This website does <b>NOT</b> save any entered name, although, it saves the result but only appear
- as digits from 0 - 5 as illustrated below: </span></div>
-	</td>
-</tr>
-<tr>
-	<td style="height: 300px">
-	<div align="center">
+<div>This website does <b>NOT</b> save any entered name, although, it saves the result but only appear
+ as digits from 0 - 5 as illustrated below:</div>
+ 
 	<table width="220" id="dis" border="0">
 		<tr><td width="100">Friends</td><td>...................</td><td width="20">0</td></tr>
 		<tr><td width="100">Lovers</td><td>...................</td><td width="20">1</td></tr>
@@ -22,16 +9,7 @@
 		<tr><td width="100">Engaged</td><td>...................</td><td width="20">4</td></tr>
 		<tr><td width="100">Sweethearts</td><td>...................</td><td width="20">5</td></tr>
 	</table>
+
+	<div class="row">So what is stored inside count.txt is only a straight stream of numbers, e.g:<br/>
+		<br/><b>count.txt</b> >> 0120340123412301 ...
 	</div>
-	</td>
-</tr>
-<tr>
-	<td style="height: 100px">
-	<div align="center">
-		<span class="txts">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;So what is stored inside count.txt is only a straight stream of numbers, e.g:<br/>
-					<br/><b>count.txt</b> >> 0120340123412301 ...
-		</span>
-	</div>
-	</td>
-</tr>
-</table>
